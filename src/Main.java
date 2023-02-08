319e13b0fef1a3ff1167127283e072d1dcf5c268
Your Name <ordoananna@gmail.com>
2023-02-08 23:20:40 +0400

    src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+        int amount = 12531;
+        int mile = 20;
+        int miles = amount / mile;
+
+
+        System.out.println("Бонусные мили:");
+        System.out.println(amount/mile);
+    }
 }
+
+
