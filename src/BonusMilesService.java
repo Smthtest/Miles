3dff9d28c1b7913184475f68e1720f229a6d986b
Your Name <ordoananna@gmail.com>
2023-02-13 16:44:04 +0400

    add src/BonusMilesService.java

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int bonus;
+        int mile = 20;
+        bonus = cost / mile;
+        return bonus;
+
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 12531;
-        int mile = 20;
-        int miles = amount / mile;
-
-
-        System.out.println("Бонусные мили:");
-        System.out.println(amount/mile);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
 
